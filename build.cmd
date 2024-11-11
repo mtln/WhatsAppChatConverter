76acf24757c3b21ec8a318d08e2f24ffdb205575
Andri Krämer <akraemer@matlon.ch>
2024-11-11 15:21:16 +0100

    do not clean branch

--- build.cmd
+++ build.cmd
@@ -3,9 +3,9 @@ copy dist\WhatsAppChatConverter.exe WhatsAppChatConverter.exe
 rm -rf dist
 rm -rf build
 rm WhatsAppChatConverter.spec
-git checkout --orphan binary_releases_new
-git add WhatsAppChatConverter.exe
-git commit -m "Binary release (no history)"
-git branch -D binary_releases
-git branch -m binary_releases_new binary_releases
-git push --set-upstream --force origin binary_releases
+rem git checkout --orphan binary_releases_new
+rem git add WhatsAppChatConverter.exe
+rem git commit -m "Binary release (no history)"
+rem git branch -D binary_releases
+rem git branch -m binary_releases_new binary_releases
+rem git push --set-upstream --force origin binary_releases
