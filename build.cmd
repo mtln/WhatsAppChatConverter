25d02944a59c2c369d6a040df819fcce15fd0230
Andri Krämer <akraemer@matlon.ch>
2024-11-19 09:52:44 +0100

    binary release 0.4.0

--- build.cmd
+++ build.cmd
@@ -1,8 +1,8 @@
-pyinstaller --noconfirm --onefile --console --name "WhatsAppChatConverter" --optimize "2"  "main.py"
-copy dist\WhatsAppChatConverter.exe WhatsAppChatConverter.exe
+pyinstaller --noconfirm --onefile --console --name "chat-export" --optimize "2"  "main.py"
+copy dist\chat-export.exe chat-export.exe
 rm -rf dist
 rm -rf build
-rm WhatsAppChatConverter.spec
+rm chat-export.spec
 rem git checkout --orphan binary_releases_new
 rem git add WhatsAppChatConverter.exe
 rem git commit -m "Binary release (no history)"
